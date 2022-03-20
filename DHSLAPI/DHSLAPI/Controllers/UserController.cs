d44689b42e33fa7fc4ddf5034feb134409e1448e
Cao Tuan Anh <49505728+tacaomta@users.noreply.github.com>
2022-03-20 12:38:44 +0700

    d

--- DHSLAPI/DHSLAPI/Controllers/UserController.cs
+++ DHSLAPI/DHSLAPI/Controllers/UserController.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
 using System.Web;
 using System.Web.Http;
 using ShipyardInfo.classes;
@@ -9,11 +11,35 @@ namespace CSBAPI.Controllers
 {
     public class UserController : ApiController
     {
+        private readonly Connection con = new Connection();
+
+
         [HttpGet]
-        [Route("api/Users")]
+        [Route("api/list_users")]
         public List<User> GetAll()
         {
-            return new List<User>();
+            return con.GetAllUsers();
+        }
+
+        [HttpPost]
+        [Route("api/insert_user")]
+        public HttpResponseMessage InsertNewUser([FromBody] User user)
+        {
+            return con.InsertNewUser(user) ? Request.CreateResponse(HttpStatusCode.Created, user) : Request.CreateResponse(HttpStatusCode.Conflict);
+        }
+
+        [HttpPut]
+        [Route("api/update_user")]
+        public HttpResponseMessage UpdateUser([FromBody] User user)
+        {
+            return con.UpdateNewUser(user) ? Request.CreateResponse(HttpStatusCode.OK, user) : Request.CreateResponse(HttpStatusCode.NotFound);
+        }
+
+        [HttpDelete]
+        [Route("api/delete_user")]
+        public HttpResponseMessage DeleteUser(string id)
+        {
+            return con.DeleteUser(id) ? Request.CreateResponse(HttpStatusCode.OK, id) : Request.CreateResponse(HttpStatusCode.NotFound);
         }
     }
 }
