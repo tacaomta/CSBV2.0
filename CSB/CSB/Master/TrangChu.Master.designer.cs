716bb0857bba335e69ea4d49190d1ba273ed4e59
DuongTuyen <dvt18012000@gmail.com>
2022-03-20 22:06:17 +0700

    TUYEN 20/03

--- CSB/CSB/Master/TrangChu.Master.designer.cs
+++ CSB/CSB/Master/TrangChu.Master.designer.cs
@@ -15,12 +15,12 @@ namespace CSB.Master
     {
 
         /// <summary>
-        /// MainContent control.
+        /// ContentPlaceHolder1 control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.ContentPlaceHolder MainContent;
+        protected global::System.Web.UI.WebControls.ContentPlaceHolder ContentPlaceHolder1;
     }
 }
