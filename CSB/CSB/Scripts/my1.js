5eda588bb50e64f91c1314314f681b6b7bbbf5ed
chuthilinh189 <chuthilinh189@gmail.com>
2022-03-23 00:41:51 +0700

    Linh
    Bảng USER

---
+++ CSB/CSB/Scripts/my1.js
@@ -0,0 +1,30 @@
+﻿$(document).ready(function () {
+    var d = new Date();
+
+    $('table[id=example2]').each(function () {
+        var table1 = $(this).DataTable({
+            lengthChange: false,
+            "language": {
+                "sProcessing": "Đang xử lý...",
+                "sLengthMenu": "Xem _MENU_ mục",
+                "sZeroRecords": "Không tìm thấy thông tin phù hợp",
+                "sInfo": "Đang xem _START_ đến _END_ trong tổng số _TOTAL_ mục",
+                "sInfoEmpty": "Đang xem 0 đến 0 trong tổng số 0 mục",
+                "sInfoFiltered": "(được lọc từ _MAX_ mục)",
+                "sInfoPostFix": "",
+                "sSearch": "Tìm kiếm: ",
+                "sUrl": "",
+                "oPaginate": {
+                    "sFirst": "Đầu",
+                    "sPrevious": "Trước",
+                    "sNext": "Tiếp",
+                    "sLast": "Cuối"
+                }
+            },
+        });
+        table1.buttons().container()
+            .appendTo('this_wrapper .col-md-6:eq(0)');
+    });
+
+
+});
