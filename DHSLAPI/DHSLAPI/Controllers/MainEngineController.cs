853e79b0d5cde156653ff3e0c6f02fd1c9891323
Cao Tuan Anh <49505728+tacaomta@users.noreply.github.com>
2022-03-28 16:24:14 +0700

    engine

--- DHSLAPI/DHSLAPI/Controllers/MainEngineController.cs
+++ DHSLAPI/DHSLAPI/Controllers/MainEngineController.cs
@@ -2,6 +2,8 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
 using System.Web;
 using System.Web.Http;
 
@@ -16,6 +18,28 @@ namespace DHSLAPI.Controllers
         public List<MainEngine> GetAll(string imo)
         {
             return con.GetMainEngineByIMO(imo);
+        }
+
+        [HttpPost]
+        [Route("api/insert_main_engine")]
+        public HttpResponseMessage InsertMainEngine([FromBody] MainEngine engine)
+        {
+            return con.InsertNewMainEngine(engine) ? Request.CreateResponse(HttpStatusCode.Created, engine
+                ) : Request.CreateResponse(HttpStatusCode.Conflict);
+        }
+
+        [HttpPut]
+        [Route("api/update_main_engine")]
+        public HttpResponseMessage UpdateMainEngine([FromBody] MainEngine engine)
+        {
+            return con.UpdateMainEngine(engine) ? Request.CreateResponse(HttpStatusCode.OK, engine) : Request.CreateResponse(HttpStatusCode.NotFound);
+        }
+
+        [HttpDelete]
+        [Route("api/delete_main_engine")]
+        public HttpResponseMessage DeleteMainEngine([FromUri] string id)
+        {
+            return con.DeleteMainEngine(id) ? Request.CreateResponse(HttpStatusCode.OK, id) : Request.CreateResponse(HttpStatusCode.NotFound);
         }
     }
 }
