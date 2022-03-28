f54e8e7d2fbb9cd165b0bceedb34863461c56b04
DuongTuyen18 <dvt18012000@gmail.com>
2022-03-28 16:31:33 +0700

    Tuyen 03/28

--- CSB/CSB/Page_Master/Manage_user.aspx.designer.cs
+++ CSB/CSB/Page_Master/Manage_user.aspx.designer.cs
@@ -13,14 +13,5 @@ namespace CSB.Page_Master
 
     public partial class Manage_user
     {
-
-        /// <summary>
-        /// form1 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
     }
 }
