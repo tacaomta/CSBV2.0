fbf92b77fd1c9b0d53d6cd7f11ef0e8d7dfeb7af
DuongTuyen18 <79651532+DuongTuyen18@users.noreply.github.com>
2023-01-30 14:54:28 +0700

    tuyen

--- CSB/CSB/obj/Release/Package/PackageTmp/value.js
+++ CSB/CSB/obj/Release/Package/PackageTmp/value.js
@@ -1,5 +1,5 @@
 ﻿var trigger = 400;
 var limit = 500;
-var linkapi = "http://localhost:8082/api/";
-//var baseaddress = "http://localhost:44347/";
-var baseaddress = "http://localhost:8080/";
+var linkapi;
+var baseaddress;
+//var baseaddress = "http://localhost:8080/";
