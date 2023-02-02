fbe963bc24f230d9f6b4cb2e4c76b8e0647d8209
DuongTuyen18 <79651532+DuongTuyen18@users.noreply.github.com>
2023-02-02 10:06:33 +0700

    tuyen

--- CSB/CSB/value.js
+++ CSB/CSB/value.js
@@ -1,5 +1,5 @@
 ﻿var trigger = 400;
 var limit = 500;
-var linkapi = "http://localhost:8082/";
-var baseaddress = "http://localhost:44347/";
-//var baseaddress;
+var linkapi = "http://localhost:8082/api/";
+//var baseaddress = "http://localhost:44347/";
+var baseaddress = "http://localhost:8080/";
